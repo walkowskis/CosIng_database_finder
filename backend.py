b9a4bdf77a04d046a63d17d31b36bbe7e8942c77
walkowskis <46692044+walkowskis@users.noreply.github.com>
2019-03-13 09:58:43 +0100

    Update backend.py

--- backend.py
+++ backend.py
@@ -1,6 +1,8 @@
 import sqlite3
+
+
 class Database:
-    def __init__(self,db):
+    def __init__(self, db):
         self.conn = sqlite3.connect(db)
         self.cur = self.conn.cursor()
         self.cur.execute("""CREATE TABLE IF NOT EXISTS cosing 
@@ -9,17 +11,15 @@ class Database:
         Restriction TEXT, Function TEXT, Update_Date TEXT)""")
         self.conn.commit()
 
-
     def view(self):
         self.cur.execute("SELECT * FROM cosing")
         rows = self.cur.fetchall()
         return rows
 
     def search(self, inci=""):
-        self.cur.execute("SELECT * FROM cosing WHERE INCI_name = ? OR CAS_No = ?", (inci, inci))
+        self.cur.execute("SELECT * FROM cosing WHERE INCI_name like ? OR CAS_No like ?", (inci, inci))
         rows = self.cur.fetchall()
         return rows
-        print(self.cur.execute("SELECT * FROM cosing WHERE INCI_name = ? OR CAS_No = ?", (inci, inci)))
-   #destructor-->now we close the connection to our database here
+
     def __del__(self):
         self.conn.close()
