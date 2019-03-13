b068b5d7d9de640f28c540fa0018412b7c8b89ed
walkowskis <46692044+walkowskis@users.noreply.github.com>
2019-03-13 09:57:54 +0100

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -1,10 +1,14 @@
 from tkinter import *
 from backend import Database
+from update import Updating, date
+from tkinter import messagebox
 
-database = Database("testowa.db")
+database_name = "testowa.db"
+database = Database(database_name)
+
 
 class Window(object):
-    def __init__(self,window):
+    def __init__(self, window):
         self.window = window
         self.window.wm_title("CosIng Searcher")
 
@@ -14,14 +18,15 @@ class Window(object):
         self.title_text = StringVar()
         self.e1 = Entry(window, textvariable=self.title_text)
         self.e1.grid(row=0, column=1)
+        self.e1.focus()
 
         self.detail_text = ''
-        self.detail = Text(window, height=7, width=45, pady=3, padx=2, wrap=WORD)
+        self.detail = Text(window, height=10, width=44, pady=3, padx=2, wrap=WORD)
         # tag configure to easy format some part of text
         self.detail.tag_configure('big', font=('Verdana', 10, 'bold'))
         self.detail.tag_configure('bold', font=('Verdana', 8, 'bold'), foreground='blue')
         self.detail.insert(END, self.detail_text)
-        self.detail.grid(row=8, column=0, rowspan=7, columnspan=4)
+        self.detail.grid(row=8, column=0, rowspan=10, columnspan=4)
 
         self.list1 = Listbox(window, height=6, width=40, font=('Tahoma', 7))
         self.list1.grid(row=1, column=0, rowspan=6, columnspan=2)
@@ -37,16 +42,19 @@ class Window(object):
 
         sb2 = Scrollbar(window, command=self.detail.yview)
         self.detail.configure(yscrollcommand=sb2.set)
-        sb2.grid(row=8, column=6, rowspan=7, sticky=W+N+S) #sticky=N+S - rozciągnięcie na wysokość
+        sb2.grid(row=8, column=6, rowspan=10, sticky=W+N+S)  # sticky=N+S - stretch to the height
 
-        b1 = Button(window, text="View all", width=12, command=self.view_command)
+        b1 = Button(window, text='View all', width=12, command=self.view_command)
         b1.grid(row=1, column=3)
 
-        b2 = Button(window, text="Search", width=12, command=self.search_command)
+        b2 = Button(window, text='Search', width=12, command=self.search_command)
         b2.grid(row=0, column=3)
 
-        b6 = Button(window, text="Close", width=12, command=window.destroy)
+        b6 = Button(window, text='Info', width=12, command=self.update)
         b6.grid(row=2, column=3)
+
+        # assigning the Enter key to a Search button:
+        self.window.bind("<Return>", lambda event: self.search_command())
 
     def empty(self):
         if len(self) > 0:
@@ -54,14 +62,7 @@ class Window(object):
         else:
             return 'N/A'
 
-    # def empty_rest(self):
-    #     if len(self) > 0:
-    #         return self.detail.insert(END, 'Restriction: ', 'bold')
-    #         self.detail.insert(END, Window.empty(self.selected_tuple[5])+'\n')
-    #     else:
-    #         return ''
-
-    def get_selected_row(self,event):
+    def get_selected_row(self, event):
         try:
             index = self.list1.curselection()[0]
             self.selected_tuple = self.list1.get(index)
@@ -75,7 +76,7 @@ class Window(object):
             self.detail.insert(END, (self.selected_tuple[8]).title()+'\n')
             self.detail.insert(END, 'Description: ', 'bold')
             self.detail.insert(END, (self.selected_tuple[6]).title())
-            
+
         except IndexError:
             # when listbox is empty
             pass
@@ -85,6 +86,30 @@ class Window(object):
         for row in database.view():
             self.list1.insert(END, row)
 
+    def update(self):
+        # if Updating(database_name).last_updating() == Updating(database_name).present_db():
+        #     claim = "\nApplication database is up-date."
+        # else:
+        #     claim = "\nA new version of the database is available, do you want to update?"
+        # answer = messagebox.askokcancel("update", "Last update date: " + Updating(database_name).last_updating() +
+        #                                 "\nAvailable database version: " + date() +
+        #                                 "\nApplication database version : " + Updating(database_name).present_db())
+        # print(claim)
+        # if answer:
+        #     Updating(database_name).update()
+        if date() == Updating(database_name).present_db():
+            answer = messagebox.showinfo("update", "Last update date: " + Updating(database_name).last_updating() +
+                                            "\nAvailable database version: " + date() +
+                                            "\nApplication database version : " + Updating(database_name).present_db() +
+                                            "\n\nApplication database is up-to-date.")
+        else:
+            answer = messagebox.askokcancel("update", "Last update date: " + Updating(database_name).last_updating() +
+                                            "\nAvailable database version: " + date() +
+                                            "\nApplication database version : " + Updating(database_name).present_db() +
+                                            "\n\nA new version of the database is available, do you want to update?")
+            if answer:
+                Updating(database_name).update()
+
     def search_command(self):
         # def empty(x):
         #     if len(str(x)) == 0:
@@ -92,10 +117,10 @@ class Window(object):
         #     else:
         #         return x
         self.list1.delete(0, END)
-        for row in database.search(self.title_text.get()):
+        for row in database.search('%'+self.title_text.get()+'%'):
             self.list1.insert(END, row)
+
 
 window = Tk()
 Window(window)
-
 window.mainloop()
