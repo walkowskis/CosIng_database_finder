19861cb82d4cc65fd56208fd13a8d58b9fc21be5
walkowskis <46692044+walkowskis@users.noreply.github.com>
2019-03-13 09:59:28 +0100

    Create update.py
    new module

---
+++ update.py
@@ -0,0 +1,92 @@
+import sqlite3
+import csv
+import urllib.request
+import os
+from datetime import date
+from tkinter import *
+from tkinter import messagebox
+
+
+today = str(date.today())
+
+# def mb_info():
+#     answer = messagebox.askokcancel("update", "Last updating: "+Updating("testowa.db").last_updating()+
+#                                     "\nPresent database version: "+date()+
+#                                     "\nCurrent databases: "+Updating("testowa.db").present_db())
+#     if (answer):
+#         Updating("testowa.db").update()
+
+class Updating:
+    def __init__(self, db):
+        self.conn = sqlite3.connect(db)
+        self.cur = self.conn.cursor()
+
+    def update(self):
+        # Download CSV file
+        url = "http://ec.europa.eu/growth/tools-databases/cosing/pdf/COSING_Ingredients-Fragrance%20Inventory_v2.csv"
+        urllib.request.urlretrieve(url, "db_update.csv")
+
+        # Drop and create the table
+        self.cur.execute('DROP TABLE IF EXISTS cosing')
+        self.cur.execute(
+            'CREATE TABLE cosing (Ref_No, INCI_name, INN, PhEur, CAS_No, EC_No, Chem_Name, Restriction, Function, Update_Date)')
+        self.conn.commit()
+
+        # Load the CSV file into CSV reader
+        with open('db_update.csv', encoding="utf8") as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            all(next(csv_file) for i in range(9))
+            for t in csv_reader:
+                try:
+                    self.cur.execute('INSERT INTO cosing VALUES (?,?,?,?,?,?,?,?,?,?)', t)
+                except sqlite3.ProgrammingError as e:
+                    pass
+
+        with open('db_update.csv', encoding="utf8") as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            date_row = [row for idx, row in enumerate(csv_reader) if idx == 6]
+            current_update = (str(date_row[0][1]))[-10:]
+            self.cur.execute("UPDATE update_data SET Data = ?, Last = ?", (today, current_update))
+            
+        # Close the csv file, commit changes, and close the connection
+        csv_file.close()
+        self.conn.commit()
+
+    def present_db(self):
+        self.cur.execute('SELECT Last FROM update_data where No=1')
+        rows = self.cur.fetchall()
+        return rows[0][0]
+
+    def last_updating(self):
+        self.cur.execute('SELECT Data FROM update_data where No=1')
+        row = self.cur.fetchall()
+        return row[0][0]
+
+    def __del__(self):
+        self.conn.close()
+
+def date():
+    av_date = ""
+    url = "http://ec.europa.eu/growth/tools-databases/cosing/pdf/COSING_Ingredients-Fragrance%20Inventory_v2.csv"
+    urllib.request.urlretrieve(url, "date.csv")
+
+    with open('date.csv', encoding="utf8") as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        # Print specific rows:
+        # interestingrows = [row for idx, row in enumerate(csv_reader) if idx in (6, 11)]
+        # Print one specific row:
+        interestingrows = [row for idx, row in enumerate(csv_reader) if idx == 6]
+        date = (str(interestingrows[0][1]))[-10:]
+
+    if os.path.exists("date.csv"):
+        os.remove("date.csv")
+    else:
+        pass
+
+    return date
+
+# root = Tk()
+# 
+# b = Button(root, text='update', command=call)
+# b.pack()
+# root.mainloop()
